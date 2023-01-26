26aac583def06b9a5401c7171ffb846090f802c9
prencelloren <120719102+prencelloren@users.noreply.github.com>
2023-01-26 17:12:45 +0800

    Update student_login.js

--- src/auth/student_login.js
+++ src/auth/student_login.js
@@ -21,7 +21,7 @@ export const Signupscreen = () => {
         } else {
             studentLogin(email, password);
             if (state.email == email && state.password == password) {
-                history.push({ pathname: "/studentpage" })
+                history.push({ pathname: "/" })
             } else {
 
             }
